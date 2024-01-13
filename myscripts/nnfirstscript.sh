d1646f51541713a9da9281e068e43490e2528a50
nafi-ullah <shafinnafiullah@gmail.com>
2024-01-13 07:58:59 +0600

    Initial Files

---
+++ myscripts/nnfirstscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "This is the first Script"
+echo
+echo "Memory usage"
+echo "##################################################"
+free -m
+echo "The Uptime of the system"
+echo "#################################################"
+uptime
+echo "Disk Utilization of System"
+echo "##############################################"
+df -h
